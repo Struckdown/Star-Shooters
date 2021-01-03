5afd0cae4356977807a53720d9495f48296850e5
Struckdown <bfleysher@gmail.com>
2021-01-02 19:03:28 -0700

    Added wave bullets

--- src/Hostiles/Bullets/Bullet_Base.gd
+++ src/Hostiles/Bullets/Bullet_Base.gd
@@ -1,17 +1,32 @@
 extends Node2D
 
-var moveSpeed = 200
+export(int) var moveSpeed = 200
+export(int) var waveStr = 0
+onready var startTime = OS.get_ticks_msec()
+var elapsedTime
+var horizontalOffset = 0
+var prevHorizontalOffset = 0
+export(int) var waveSpeed = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	waveSpeed *= 10
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var forwardVec = Vector2(1, 0).rotated(rotation).normalized()
-	position += forwardVec * moveSpeed * delta
+	move(delta)
 
 
 func _on_DespawnTimer_timeout():
 	queue_free()
+
+func move(delta):
+	var forwardVec = Vector2(1, 0).rotated(rotation).normalized()
+	var sideVec = transform.y
+	elapsedTime = OS.get_ticks_msec() - startTime
+	horizontalOffset = sin(elapsedTime/waveSpeed) * waveStr
+	sideVec *= (horizontalOffset - 0)
+	position += forwardVec * moveSpeed * delta	# forward motion
+	position += sideVec * delta	# side wiggle
+	prevHorizontalOffset = horizontalOffset
